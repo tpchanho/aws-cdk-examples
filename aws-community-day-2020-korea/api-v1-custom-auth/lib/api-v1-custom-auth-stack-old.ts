10c77abc9d25b32b42a6c341ac982f59dc5d4d9f
tpchanho <tpchanho@gmail.com>
2020-10-07 22:12:35 +0900

    cfn authorizer 대신 request, token authorizer 추가

--- aws-community-day-2020-korea/api-v1-custom-auth/lib/api-v1-custom-auth-stack-old.ts
+++ aws-community-day-2020-korea/api-v1-custom-auth/lib/api-v1-custom-auth-stack-old.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-import { RestApi, CfnAuthorizer, LambdaIntegration, AuthorizationType, CfnMethod, RequestAuthorizer, TokenAuthorizer } from '@aws-cdk/aws-apigateway'
+import { RestApi, CfnAuthorizer, LambdaIntegration, AuthorizationType, CfnMethod } from '@aws-cdk/aws-apigateway'
 import { Function, Runtime, AssetCode } from '@aws-cdk/aws-lambda'
 import { Role, ServicePrincipal } from '@aws-cdk/aws-iam'
 
@@ -12,26 +12,23 @@ export class ApiV1CustomAuthStack extends cdk.Stack {
     const authorizerFunc = new Function(this, 'Sample01AuthFunc', {
       functionName: 'Sample01AuthFunc',
       code: new AssetCode('./lambda-src/authorizer'),
-      // handler: 'authorizer_rq.handler',
-      handler: 'authorizer_tk.handler',
+      handler: 'authorizer.handler',
       runtime: Runtime.PYTHON_3_8,
     })
-
     const role = new Role(this, 'Sample01AuthorizerRole', {
       assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
     });
 
     authorizerFunc.grantInvoke(role);
-    // const authorizer = new RequestAuthorizer(this, 'Sample01RequestAuthorizer', {
-    const authorizer = new TokenAuthorizer(this, 'Sample01TokenAuthorizer', {
-      handler: authorizerFunc,
-      // identitySources: ['method.request.header.Authorization'],
-      identitySource: 'method.request.header.Authorization',
-      assumeRole: role,
-      // authorizerName: 'Sample01RequestAuthorizer',
-      authorizerName: 'Sample01TokenAuthorizer',
-    })
 
+    const authorizer = new CfnAuthorizer(this, 'Sample01Authorizer', {
+      name: 'Sample01Authorizer',
+      restApiId: api.restApiId,
+      type: 'TOKEN',
+      identitySource: 'method.request.header.Authorization',
+      authorizerCredentials: role.roleArn,
+      authorizerUri: `arn:aws:apigateway:${this.region}:lambda:path/2015-03-31/functions/${authorizerFunc.functionArn}/invocations`
+    });
 
     const testResource = api.root.addResource('test');
 
@@ -45,7 +42,8 @@ export class ApiV1CustomAuthStack extends cdk.Stack {
     const testIntegration = new LambdaIntegration(testFunc);
     const testMethod = testResource.addMethod('GET', testIntegration, {
       authorizationType: AuthorizationType.CUSTOM,
-      authorizer: authorizer
     })
+    const testMethodResource = testMethod.node.findChild('Resource') as CfnMethod;
+    testMethodResource.addPropertyOverride('AuthorizerId', { Ref: authorizer.logicalId });
   }
 }
